f5477aa9678371ad77c84b431498ac3f5c1ba23e
siddhikreddy <166997192+siddhik-reddy@users.noreply.github.com>
2026-09-14 17:02:34 +0530

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,21 @@
+import Hero from "../components/Hero.jsx";
+import About from "../components/About.jsx";
+import Skills from "../components/Skills.jsx";
+import Projects from "../components/Projects.jsx";
+import Experience from "../components/Experience.jsx";
+import Education from "../components/Education.jsx";
+import Contact from "../components/Contact.jsx";
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Skills />
+      <Projects />
+      <Experience />
+      <Education />
+      <Contact />
+    </>
+  );
+}
