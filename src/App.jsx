a95f77ea10124c15ad72515dd44e4556de4d2720
siddhikreddy <166997192+siddhik-reddy@users.noreply.github.com>
2026-09-14 16:54:22 +0530

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <main className="container section">
+      <h1 className="h1">Siddhik Reddy</h1>
+      <p className="lead">Frontend Developer</p>
+    </main>
+  );
+}
