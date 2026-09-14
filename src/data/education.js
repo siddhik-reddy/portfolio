284bdfaa4af11a2c1025f12e09e934894f78500a
siddhikreddy <166997192+siddhik-reddy@users.noreply.github.com>
2026-09-14 17:15:35 +0530

    Update education.js

--- src/data/education.js
+++ src/data/education.js
@@ -4,13 +4,13 @@ export const education = [
     degree: "B.Tech, Computer Science",
     institution: "Malla Reddy Institute of Technology and Science",
     period: "2021 — 2025",
-    location: "Hyderabad, Telangana",
+    location: "India",
   },
   {
     degree: "Higher Secondary Education (XII)",
     institution: "Narayana Junior College",
     period: "2019 — 2021",
-    location: "Hyderabad, Telangana",
+    location: "India",
   },
 ];
 ```
